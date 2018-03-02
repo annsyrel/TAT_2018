e7c8dfdad86d79b0e2058be61be88d241694746d
annsyrel <ajutas.96@mail.ru>
2018-03-02 16:53:50 +0300

    Added faculty

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hanna Syrel, 4 course, group 2+4+8.");
+            Console.ReadKey();
+        }
+    }
+}
