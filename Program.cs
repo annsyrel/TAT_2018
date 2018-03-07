d044af0605313d16141961204e5fb1eb80672380
annsyrel <36953654+annsyrel@users.noreply.github.com>
2018-03-07 17:01:36 +0300

    task-3

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DEV3
+{
+    class DEV3
+    {
+         static void Main(string[] args)
+        {
+            int Number, Redix;
+            try
+            {
+                if (args.Length != 2 || args == null)
+                {
+                    throw new Exception("Wrong number of argumets.");
+                }
+                Number = int.Parse(args[0]);
+                Redix = int.Parse(args[1]);
+                if(Redix <=1 || Redix >= 21)
+                {
+                    throw new Exception("Invalid redix.");
+                }
+                ConvertFrom10ToAnyRedix.Converte(Number, Redix);
+            }
+            catch (ArgumentException)
+            {
+                Console.Write("Write your numbers");
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            };
+        }
+    }
+}
