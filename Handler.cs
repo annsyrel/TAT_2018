389fc112ce88fafda7023ac1184f303d497c1aab
annsyrel <36953654+annsyrel@users.noreply.github.com>
2018-04-18 17:51:50 +0300

    Syrel

---
+++ Handler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV8
+{
+    class Handler
+    {
+        public void SimilarLastName(Man i)
+        {
+            Console.Write("Last name: ");
+            Console.WriteLine(i.LastName);
+            Console.Write("First name: ");
+            Console.WriteLine(i.FirstName);
+            Console.Write("Gender: ");
+            Console.WriteLine(i.Gender);
+            Console.Write("Age: ");
+            Console.WriteLine(i.Age);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {            
+            Operations o = new Operations();
+            Handler Handler1 = new Handler();
+            o.onCount += Handler1.SimilarLastName;
+            var m = new List<Man>();
+            bool a = true;
+            while (a)
+            {
+                Man i = new Man();
+                m.Add(i);
+                o.LastNames(m);
+                Console.WriteLine("Do U want to add person? ");
+                if (Console.ReadLine() == "no")
+                {
+                    a = false;
+                }
+            }
+            o.MiddleAge(m);
+            o.OlderUser(m);
+            o.PopularName(m);
+            Console.ReadKey();
+        }
+    }
+}
