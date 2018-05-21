8ae846e2a3601d515b8da2219516c4d52f64d26d
annsyrel <36953654+annsyrel@users.noreply.github.com>
2018-05-21 21:50:37 +0300

    Add files via upload
    task 1

---
+++ EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace task_1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("And hi again");
+            Random rnd = new Random();
+            int value = rnd.Next(5, 50);
+            for(int i=0; i<value; i++)
+            {
+                Console.Write("!");
+            }
+        }
+    }
+}
